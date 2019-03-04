c8165f816abf2db6c84d96355a20de80b7b05423
iasjem <jemimah.beryl.sai@gmail.com>
2019-03-04 21:42:02 +0800

    Add new routes for about and post page

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,6 +2,8 @@ const express = require('express'),
     router = express.Router();
 
 router.get('/', (req, res) => res.render('index.hbs'));
+router.get('/about', (req, res) => res.render('about.hbs'));
+router.get('/posts/:id', (req, res) => res.render('post.hbs'));
 router.get('*', (req, res) => res.status(400).render('404.hbs'));
 
 module.exports = router;
