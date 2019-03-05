d080167a05a528f483a81a02822b44541b5da98f
iasjem <jemimah.beryl.sai@gmail.com>
2019-03-05 16:38:28 +0800

    Remove unused properties of About object model

--- server/models/About.js
+++ server/models/About.js
@@ -3,10 +3,6 @@ const { convertMD }  = require('../scripts/convertMD'),
 
 const About = {
     filename: 'author-intro',
-    title: 'About Inquisitive Jane',
-    overview: 'About the author',
-    author: 'iasjem',
-    createdDate: new Date('2019-03-03'),
     mdFile() { 
         return convertMD(path.join(`${ __dirname }/../posts/${ this.filename }`));
     }
